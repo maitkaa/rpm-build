3992921fbb947651995ce2360e0c05da5efcec4b
Mait Kaasik <mait.kaasik@gmail.com>
2024-10-01 12:03:36 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,6 +58,7 @@ echo "Copying files to BUILDROOT..."
 
 echo $PROJECT_EXCLUDE_PATHS | tr ',' '\n' > excluded_paths.txt
 rsync -a --exclude-from=./excluded_paths.txt "$WORKSPACE/" "$RPM_BUILD_ROOT$APPROOT/"
+rm excluded_paths.txt
 
 if [ "$RUN_LINT" = "true" ]; then
     echo "Running rpmlint..."
